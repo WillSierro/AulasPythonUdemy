88d3cd01a21ebd4e1f51e7a6dadd8cfae264fc18
William Sierro <willsierro@gmail.com>
2023-06-14 09:45:40 -0300

    first commit

---
+++ aulasPrimariasPy.py
@@ -0,0 +1,42 @@
+# print("Hello, World!")
+
+# print(11) #int
+# print(-11) #int
+# print(1.1) #float
+# print(type('William'))
+# print(type(1))
+# print(type(1.1), type(-1.1), type(1), type(True))
+# print(10 == 10) #True or False
+# print(int('1') + 1)
+# nome = input("Seu nome? ")
+# sobrenome = input('Seu sobrenome? ')
+# idade = int(input('Sua idade? '))
+# ano_nascimento = 2023 - idade
+# maior_de_idade = idade >= 18
+# altura_metros = float(input("Sua altura? "))
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de Nascimento:', ano_nascimento)
+# print('É maior de idade?:', maior_de_idade)
+# print('Altura em metros:', altura_metros)
+
+#Projeto IMC
+# nome = "William Sierro"
+# peso = 205
+# altura = 1.86
+# imc = peso / (altura * altura)
+
+# print(nome, 'tem ', altura, 'de altura e pesa ', peso, 'quilos e seu IMC é de ', imc )
+# print(f'{nome} tem {altura} de altura e pesa {peso} quilos e seu IMC é de {imc}')
+# print('{} tem {} de altura e pesa {} quilos e seu IMC é de {}' .format(nome, altura, peso, imc))
+
+# nome = input('qual seu nome? ')
+# print(f'seu nome é {nome=}') #nome= irá aparecer no proprio compilador como nome=
+
+# # nome = int(input("digite um numero")) #não fazer isso pois mata o programa sem saber o que o usuario digitou 
+
+# numero = input('Digite um numero: ')
+# int_numero = int(numero)
+
+# print(int_numero)
